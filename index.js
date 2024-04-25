5a717b1cd4ea5029889cfa48677f0aa58bc3992e
qnrs <2116377053@qq,com>
2024-04-25 15:23:42 +0800

    添加静态文件

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 window.addEventListener('load', function () {
-    if (window.localStorage.getItem('dark-mode').toString() === "true") {
+    if (!window.localStorage.getItem('dark-mode') === null&&window.localStorage.getItem('dark-mode').toString() === "true") {
         setTheme();
     }
     L2Dwidget.init({
@@ -45,7 +45,7 @@ function getRandomIntExclude(min, max, exclude) {
 function generateRandomNumber() {
     var min = document.getElementById('min').value; //从输入框获取最小值
     var max = document.getElementById('max').value; //从输入框获取最大值
-    var number = getRandomIntExclude(min, max, [31, 36, 46]);  //生成一个在指定范围内的随机数
+    var number = getRandomIntExclude(min, max, [19, 31, 36, 46]);  //生成一个在指定范围内的随机数
     document.getElementById('randomNumber').innerHTML = number; //在网页上显示这个随机数
 }
 
