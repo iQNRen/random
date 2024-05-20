39a124d03b9765061f960cbba4f2d04cbb85ebe0
李同学 <95062435+iQNRen@users.noreply.github.com>
2024-05-20 09:32:46 +0800

    youhua

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function getRandomIntExclude(min, max, exclude) {
 function generateRandomNumber() {
     var min = document.getElementById('min').value; //从输入框获取最小值
     var max = document.getElementById('max').value; //从输入框获取最大值
-    var number = getRandomIntExclude(min, max, [19, 31, 35,36, 40, 46, 49, 61]);  //生成一个在指定范围内的随机数
+    var number = getRandomIntExclude(min, max, [19, 31, 35, 36, 40, 46, 49, 61]);  //生成一个在指定范围内的随机数
     document.getElementById('randomNumber').innerHTML = number; //在网页上显示这个随机数
 }
 
