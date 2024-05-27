84cfa4ff59823239fbe28a091ea45abf65b014ee
qnrs <2116377053@qq,com>
2024-05-27 13:37:36 +0800

    优化，降低重复率

--- index.js
+++ index.js
@@ -3,31 +3,6 @@ window.addEventListener('load', function () {
         setTheme();
     }
 });
-
-function getRandomIntExclude(min, max, exclude) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    if (min === max && min != '' && max != '') {
-        window.confirm("最小值和最大值不能相同")
-        return 0;
-    } else if (min > max) {
-        window.confirm("最小值不能大于最大值")
-        return 0;
-    }
-    var randomNumber;
-    do {
-        randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-    } while (exclude.includes(randomNumber));
-    return randomNumber;
-}
-
-function generateRandomNumber() {
-    var min = document.getElementById('min').value; //从输入框获取最小值
-    var max = document.getElementById('max').value; //从输入框获取最大值
-    var number = getRandomIntExclude(min, max, [19, 31, 35, 36, 40, 44, 46, 49, 61]);  //生成一个在指定范围内的随机数
-    document.getElementById('randomNumber').innerHTML = number; //在网页上显示这个随机数
-}
-
 function setTheme() {
     var html = document.getElementsByTagName('html')[0]
     // 检查html元素的theme属性是否为dark-mode
@@ -43,6 +18,43 @@ function setTheme() {
         window.localStorage.setItem('dark-mode', true)
     }
 };
+
+
+function getRandomIntExclude(min, max, exclude) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    if (min === max && min != '' && max != '') {
+        window.confirm("最小值和最大值不能相同")
+        return 0;
+    } else if (min > max) {
+        window.confirm("最小值不能大于最大值")
+        return 0;
+    }
+    var randomNumber;
+    var cnt = 0;
+    do {
+        randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+        cnt++;
+        if(cnt>(max-min)){
+            randomNumber = 0;
+            excludeSet = new Set(excludeNum)
+            break;
+        }
+    } while (exclude.has(randomNumber));
+    return randomNumber;
+}
+
+var excludeNum = [19, 31, 35, 36, 40, 44, 46, 49, 61]
+var excludeSet = new Set(excludeNum);
+
+function generateRandomNumber() {
+    var min = document.getElementById('min').value; //从输入框获取最小值
+    var max = document.getElementById('max').value; //从输入框获取最大值
+    var number = getRandomIntExclude(min, max, excludeSet);  //生成一个在指定范围内的随机数
+    excludeSet.add(number); //将这个随机数添加到排除集合中
+    document.getElementById('randomNumber').innerHTML = number; //在网页上显示这个随机数
+}
+
 
 particlesJS.load('particles-js', 'assets/particles.json', function () {
     console.log('callback - particles.js config loaded');
